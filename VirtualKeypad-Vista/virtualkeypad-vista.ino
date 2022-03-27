fedee2951a6eeb6a366f0fde8f2af7aed655a6fc
Alain Turbide <aturbide99@gmail.com>
2022-03-27 14:33:46 -0400

    - added partition filtering

--- VirtualKeypad-Vista/virtualkeypad-vista.ino
+++ VirtualKeypad-Vista/virtualkeypad-vista.ino
@@ -666,6 +666,8 @@ void loop() {
       }
     }
     if (vista.cbuf[0] == 0xF7 && vista.newCmd) {
+      int kpaddrbit=0x01 << (kpaddr - 16);
+      if (!(vista.cbuf[3] & kpaddrbit)) return; // not addressed to this keypad        
       memcpy(p1, vista.statusFlags.prompt, 16);
       memcpy(p2, & vista.statusFlags.prompt[16], 16);
       p1[16] = '\0';
